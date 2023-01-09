2efb64dee57446dcc121e4ec2e31a4350091b462
Kieran Cline <clinekieran@gmail.com>
2023-01-09 13:56:25 -0500

    Initial commit

---
+++ test.py
@@ -0,0 +1,33 @@
+# Simple pygame program
+
+# Import and initialize the pygame library
+import pygame
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# Variable to keep the main loop running
+running = True
+
+# Main loop
+while running:
+    for event in pygame.event.get():
+        # Did the user hit a key?
+        if event.type == KEYDOWN:
+            # Was it the Escape key? If so, stop the loop.
+            if event.key == K_ESCAPE:
+                running = False
+                # Did the user click the window close button? If so, stop the loop.
+        elif event.type == QUIT:
+            running = False
+
