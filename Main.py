9b2bad69fb45b7f8e4d9a4b9f4860eb4655743c1
Kieran Cline <clinekieran@gmail.com>
2023-03-01 14:28:12 -0500

    Commented stuf

--- Main.py
+++ Main.py
@@ -3,10 +3,11 @@ from pygame import mixer
 import math
 from Procedural_Generator import ProceduralGenerator
 from Classes import Player
-
+#sets up pygame and mixer
 pygame.init()
 mixer.init()
 
+#sets up temp background music
 mixer.music.load('Music/Character_Menu.mp3')
 mixer.music.play(-1)
 mixer.music.set_volume(0.10)
@@ -16,10 +17,13 @@ mixer.music.set_volume(0.10)
 ScreenLength = 1870
 ScreenWidth = 1030
 
+#sets up the generator class with tile size
 generator = ProceduralGenerator(int(ScreenLength / 10), int(ScreenWidth / 10))
 
+#generates map
 generator.Generate(5, 10, 30, 5, True, 50, 10, 2, None, 1, 2, 5, 25)
 
+#inits player
 player = Player((generator.startloc[0][0], generator.startloc[0][1]))
 
 # Set up the drawing window
@@ -29,9 +33,13 @@ pygame.display.set_caption('The Lost Dungeon')
 
 screen.fill((0, 0, 0))
 obstructions = []
+
+#draws map
 map = generator.DrawMap(map, obstructions)
 
+#hides mouse
 pygame.mouse.set_visible(False)
+#loads image
 direction_indicator = pygame.transform.scale_by(pygame.image.load("Art/Character/Direction-Temp.png"), 1 / 6).convert()
 direction_indicator.set_colorkey((255,255,255))
 direction_indicator_rect = direction_indicator.get_rect(center= (ScreenLength/2, ScreenWidth/2))
@@ -39,6 +47,7 @@ direction_indicator_rect = direction_indicator.get_rect(center= (ScreenLength/2,
 # Run until the user asks to quit
 running = True
 
+#sets up main verables
 camera_X = 0 - generator.startloc[0][0] * 5 + ScreenLength / 2 - 10
 camera_Y = 0 - generator.startloc[0][1] * 5 + ScreenWidth / 2 - 10
 cameraSpeed = 1
@@ -60,20 +69,23 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        #if key goes up
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_m:
                 maptriggered = False
         elif event.type == pygame.KEYDOWN:
+            #if user presses M open map
             if event.key == pygame.K_m:
                 if mapopen and not maptriggered:
                     mapopen = False
                 elif not mapopen:
                     mapopen = True
             elif event.key == pygame.K_ESCAPE:
+                running = False
                 pygame.quit()
 
 
-
+        #if player moves the mouse calculate new angle for the direction indicator
         elif event.type == pygame.MOUSEMOTION and not skipmousecheck:
             mouse_x, mouse_y = event.pos
             mouse_rel_x, mouse_rel_y = event.rel
@@ -84,7 +96,7 @@ while running:
             else:
                 angle += math.ceil(mouse_rel_y/5)
 
-
+    #gets all keys and does movment for the player.
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_LEFT] or keys[pygame.K_a]:
@@ -136,22 +148,28 @@ while running:
     if playerLocation == (ScreenLength / 2 - 25, ScreenWidth / 2 - 25):
         moveTime = 1
 
-    direction_indicator_rotated = pygame.transform.rotate(direction_indicator, angle)
+    #rotates direction indicator based off angle
+    direction_indicator_rotated = pygame.transform.rotate(direction_indicator, -angle)
     rect = direction_indicator_rotated.get_rect(center=direction_indicator_rect.center)
 
     # Fill the background with white
     screen.fill((0, 0, 0))
+    #if map is closed draw game window to screen
     if not mapopen:
         screen.blit(BIGmap, (camera_X, camera_Y))
         screen.blit(player.image, (ScreenLength / 2 - 10, ScreenWidth / 2 - 10))
         screen.blit(direction_indicator_rotated, rect)
+    #otherwise draw map
     else:
         screen.blit(map, (0, 0))
         screen.blit(player.tinyimage, playerLocation)
+
+    #updates playerlocation
     player.rect.x = playerLocation[0]
     player.rect.y = playerLocation[1]
     # Flip the display
     pygame.display.flip()
+    #if mouse leaves screen teleport it back to center
     mouse_x, mouse_y = pygame.mouse.get_pos()
     if mouse_x < 100 or mouse_x > ScreenLength-100 or mouse_y < 100 or mouse_y > ScreenWidth-100:
         skipmousecheck = True
